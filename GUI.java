5f33667e3851515de945f9252dd01fafd6402ed1
alexeastlake <alex.eastlake@gmail.com>
2021-08-13 15:16:08 +1200

    Added basic GUI components

--- GUI.java
+++ GUI.java
@@ -1,9 +1,26 @@
 import javax.swing.*;
 
+/**
+ * The GUI class creates and uses an interface to interact with the CrosswordSolver class
+ * 
+ * @author alexeastlake
+ */
 class GUI {
 	
+	// Primary GUI components
+	private JFrame frame;
+	private JPanel panelArea;
+	private JTextArea outputArea;
+	
 	public GUI(CrosswordSolver crosswordSolver) {
+		frame = new JFrame();
+		panelArea = new JPanel();
+		outputArea = new JTextArea();
 		
+		frame.add(panelArea);
+		frame.add(outputArea);
+		
+		frame.setVisible(true);
 	}
 	
 }
