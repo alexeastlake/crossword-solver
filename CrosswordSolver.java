f3bb9c0a6aa2080ce3ef127641a1ac623c423100
alexeastlake <alex.eastlake@gmail.com>
2021-08-23 19:20:24 +1200

    Formatting

--- CrosswordSolver.java
+++ CrosswordSolver.java
@@ -14,37 +14,31 @@ class CrosswordSolver {
 	// Map with length of word as the key to list of words of that length
 	private Map<Integer, List<char[]>> wordLists = new HashMap<Integer, List<char[]>>();
 	
-	public CrosswordSolver() {
-		this.readFile("words_alpha.txt");
-	}
-	
 	/**
 	 * Read a .txt file of words into the wordLists map
 	 * 
 	 * @param path of .txt file
+	 * @throws IOException 
 	 */
-	public void readFile(String path) {
-		// TODO: Add error handling to try catch block
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
+	public void readFile(String path) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(path));
+		String currentWord;
+		char[] currentWordChars;
+		
+		while ((currentWord = reader.readLine()) != null) {
+			currentWordChars = currentWord.toCharArray();
 			
-			String currentWord;
-			char[] currentWordChars;
-			
-			while ((currentWord = reader.readLine()) != null) {
-				currentWordChars = currentWord.toCharArray();
+			if (wordLists.containsKey(currentWordChars.length)) {
+				wordLists.get(currentWordChars.length).add(currentWordChars);
+			} else {
+				List<char[]> newLengthList = new ArrayList<char[]>();
 				
-				if (wordLists.containsKey(currentWordChars.length)) {
-					wordLists.get(currentWordChars.length).add(currentWordChars);
-				} else {
-					List<char[]> newLengthList = new ArrayList<char[]>();
-					newLengthList.add(currentWordChars);
-					wordLists.put(currentWordChars.length, newLengthList);
-				}
+				newLengthList.add(currentWordChars);
+				wordLists.put(currentWordChars.length, newLengthList);
 			}
-			
-			reader.close();
-		} catch (FileNotFoundException e) {} catch (IOException e) {}
+		}
+		
+		reader.close();
 	}
 	
 	/**
