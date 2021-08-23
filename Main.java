adb608434af765492ba853ce07c3fc508cd7c962
alexeastlake <alex.eastlake@gmail.com>
2021-08-23 19:21:49 +1200

    Formatting
    Removed unused imports

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  * Contains the main method for the Crossword Solver tool
  * 
@@ -6,6 +9,14 @@
 class Main {
 	
 	public static void main(String args[]) {
-		new GUI(new CrosswordSolver());
+		CrosswordSolver crosswordSolver = new CrosswordSolver();
+		
+		try {
+			crosswordSolver.readFile("words_alpha.txt");
+			new GUI(crosswordSolver);
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Error reading word files:\n" + e.getMessage());
+			System.exit(0);
+		}
 	}
 }
